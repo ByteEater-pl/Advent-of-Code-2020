243a8595f7740b7936222abc1dcda94726d90c0c
ByteEater <km@localhost.localdomain>
2020-12-02 05:26:37 +0100

    Solve Day 2

--- aoc.js
+++ aoc.js
@@ -1,18 +1,17 @@
 "use strict"
 
+function input(name) {
+	return require("fs").readFileSync(`${name}.in`, {encoding: "latin1"}).split("\n")
+}
+
 function day1() {
 	const
-		expenses = [],
+		expenses = input(1),
 		inds = [0],
 		attempt = () => (2020 - expenses[inds[0]] - expenses[inds[2]]) * dir
-	let n, dir = 1
-	for (;;) {
-		n = readline()
-		if (n == null) break
-		expenses.push(+n)
-	}
 	expenses.sort((a, b) => a - b)
 	inds[2] = expenses.length - 1
+	let dir = 1
 	for (;;) {
 		while (attempt() > 0)
 			inds[1 - dir] += dir
@@ -22,4 +21,24 @@ function day1() {
 	}
 }
 
-print(day1())
+function day2() {
+	const
+		expenses = input(1),
+		inds = [0, 1],
+		attempt = () =>
+			inds.reduce((a, x) => a - expenses[x], 2020) * dir
+	expenses.sort((a, b) => a - b)
+	inds[2] = expenses.length - 1
+	let dir = 1, d
+	for (;;) {
+		while ((d = inds[1 + dir] - inds[1] - dir) && attempt() > 0)
+			inds[1] += dir
+		if (attempt() == 0)
+			return inds.reduce((a, x) => a * expenses[x], 1)
+		inds[d ? 1 + dir : 1 - dir] += d ? -dir : dir
+		dir *= -1
+	}
+}
+
+console.log(day1())
+console.log(day2())

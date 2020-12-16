fb2b4c7ee42a53f8dfa8800afbd79c0208c4e531
ByteEater <km@localhost.localdomain>
2020-12-16 09:48:47 +0100

    Make add change n instead of returning, refactor

--- day15_2 (slower).js
+++ day15_2 (slower).js
@@ -1,16 +1,15 @@
 // Surprisingly, this works slower on V8 than day15_1 with the number changed.
 // But neither is fast enough.
 function day15_2() {
+	let n
 	const ages = new Map
 	function add(x) {
 		for (let [k, v] of ages)
 			ages.set(k, v + 1)
-		let n = ages.get(+x) ?? 0
+		n = ages.get(+x) ?? 0
 		ages.set(+x, 0)
-		return n
 	}
-	let n = "13,0,10,12,1,5,8".split(",").map(add).pop()
-	for (let _ = ages.size; _ < 29999999; _++)
-		n = add(n)
+	"13,0,10,12,1,5,8".split(",").forEach(add)
+	for (let _ = ages.size; _ < 29999999; _++) add(n)
 	return n
 }

ef28c71cbc82736187e5ba894a0d3cb04e72cb58
ByteEater <km@localhost.localdomain>
2020-12-16 09:37:42 +0100

    Eliminate const numbers

--- day15_2 (slower).js
+++ day15_2 (slower).js
@@ -1,9 +1,7 @@
 // Surprisingly, this works slower on V8 than day15_1 with the number changed.
 // But neither is fast enough.
 function day15_2() {
-	const
-		numbers = "13,0,10,12,1,5,8".split(","),
-		ages = new Map
+	const ages = new Map
 	function add(x) {
 		for (let [k, v] of ages)
 			ages.set(k, v + 1)
@@ -11,8 +9,8 @@ function day15_2() {
 		ages.set(+x, 0)
 		return n
 	}
-	let n = numbers.map(add).pop()
-	for (let _ = numbers.length; _ < 29999999; _++)
+	let n = "13,0,10,12,1,5,8".split(",").map(add).pop()
+	for (let _ = ages.size; _ < 29999999; _++)
 		n = add(n)
 	return n
 }
